1953b2d3f0309e29585e8d17686efc70d2123d01
John Navarrete <jwnavarretez@gmail.com>
2025-05-16 20:47:51 -0500

    currency updated

--- src/components/ui/CurrencyInputUI.tsx
+++ src/components/ui/CurrencyInputUI.tsx
@@ -1,12 +1,16 @@
 import { TextField, TextFieldProps } from "@mui/material";
 import { NumericFormat } from "react-number-format";
 
-interface CurrencyInputProps extends Omit<TextFieldProps, "onChange" | "value"> {
+interface CurrencyInputProps{
     value: number;
+    size: 'small' | 'medium' | undefined;
+    label: string;
     onChange: (value: number) => void;
+    defaultValue?: string | number | null;
 }
 
 export const CurrencyInput: React.FC<CurrencyInputProps> = ({ value, onChange, ...muiProps }) => {
+    const { defaultValue, ...rest } = muiProps;
     return (
         <NumericFormat
             value={value}
@@ -16,11 +20,20 @@ export const CurrencyInput: React.FC<CurrencyInputProps> = ({ value, onChange, .
             fixedDecimalScale
             customInput={TextField}
             fullWidth
+            className=""
             onValueChange={(values) => {
                 const { floatValue } = values;
-                onChange(floatValue || 0); // Pasar valor limpio
+                onChange(floatValue || 0);
             }}
-            {...muiProps} // Pasar las props de Material-UI
+            defaultValue={
+                typeof defaultValue === 'string' ||
+                typeof defaultValue === 'number' ||
+                defaultValue === null ||
+                defaultValue === undefined
+                    ? defaultValue
+                    : undefined
+            }
+            {...rest}
         />
     );
 };
